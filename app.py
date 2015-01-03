bf5c264818e6b617dc0e34232081c4414bfe0e17
Kohl Bromwich <kohl.bromwich@gmail.com>
2015-01-03 14:39:44 +1300

    Trying to get webserver running with gevent

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
-# coding: utf-8
+
+#
+# This file may be used instead of Apache mod_wsgi to run your python
+# web application in a different framework.  A few examples are
+# provided (cherrypi, gevent), but this file may be altered to run
+# whatever framework is desired - or a completely customized service.
+#
+import imp
+import os
+
+try:
+   zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
+                           'virtenv', 'bin', 'activate_this.py')
+   execfile(zvirtenv, dict(__file__ = zvirtenv) )
+except IOError:
+   pass
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 
-from app import app as application
+from app import my_app
 
 if __name__ == '__main__':
-	from app import my_app
-    http_server = WSGIServer(('',5000), my_app, handler_class=WebSocketHandler)
+	ip   = os.environ['OPENSHIFT_PYTHON_IP']
+	port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+	app = imp.load_source('application', 'wsgi/application')
+	
+	print 'Starting gevent WSGIServer on %s:%d ... ' % (fwtype, ip, port)
+	WSGIServer((ip, port), app.application).serve_forever()
+	
+    http_server = WSGIServer((ip, port), my_app, handler_class=WebSocketHandler)
     http_server.serve_forever()
