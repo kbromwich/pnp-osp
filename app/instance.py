a38b9f7ad24881a2ce2fa1c8e69cdd4343d65f27
Kohl Bromwich <kohl.bromwich@gmail.com>
2015-01-04 13:27:13 +1300

    Finished up basic chat functionality

--- app/instance.py
+++ app/instance.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 import json
+import datetime
+import random
+import re
 
 INSTANCES = {}
 
@@ -7,30 +10,76 @@ class Instance(object):
     
     def __init__(self, instanceId):
         self.instanceId = instanceId
-        self.clients = set([])
+        self.clients = {}
         self.history_draws = []
         self.history_texts = []
+        self.last_client_time = datetime.datetime.utcnow()
     
-    def add_client(self, ws):
-        self.clients.add(ws)
+    def add_client(self, ws, msg):
+        meta = self.extract_client_meta(ws, msg)
+        name = meta['username']
+        self.announce(name + ' has joined the room.')
+        self.clients[ws] = meta
     
     def remove_client(self, ws):
-        self.clients.remove(ws)
+        meta = self.clients[ws]
+        del self.clients[ws]
+        name = meta['username']
+        self.announce(name + ' has left the room.')
+        self.last_client_time = datetime.datetime.utcnow()
+        if len(self.clients) == 0:
+            # TODO: queue task to remove instance after 30 mins
+            pass
+            
+    def rename_client(self, ws, prev_meta):
+        prev = prev_meta['username'];
+        name = get_client_name(ws)
+        self.announce('{0} has changed their name to {1}'.format(prev, name))
+        
+    def get_client_name(self, client):
+        return self.clients[client]['username']
         
     def on_client_message(self, ws, msg):
+        prev_meta = self.clients[ws].copy();
+        self.clients[ws].update(self.extract_client_meta(ws, msg))
         if msg['type'] == 'refresh':
             self.refresh_client(ws)
+        elif msg['type'] == 'rename':
+            self.rename_client(ws, prev_meta)
         elif msg['type'] == 'text':
-            self.history_texts.append(msg)
-            for client in self.clients:
-                self.send_client_texts(client, [msg])
+            self.broadcast(msg)
         elif msg['type'] == 'draw':
             pass
         elif msg['type'] == 'dice':
-            pass
+            self.client_dice_roll(ws, msg)
+        
+    def extract_client_meta(self, ws, msg):
+        meta = {}
+        if msg.get('username') is not None:
+            meta['username'] = msg['username']
+        return meta
             
     def refresh_client(self, client):
         self.send_client_texts(client, self.history_texts)
+        
+    def client_dice_roll(self, client, msg):
+        diceMatch = re.match("^D(\d+)$", msg['dice'], flags=re.I)
+        dice = int(diceMatch.group(1))
+        roll = random.randint(1, dice)
+        self.announce('{0} rolled a D{1:d} and got {2:d}'.format(
+                        self.get_client_name(client), dice, roll))
+        
+    def announce(self, message):
+        msg = {'instanceId': self.instanceId}
+        msg['username'] = 'System'
+        msg['text'] = message
+        self.broadcast(msg)
+        
+    def broadcast(self, msg):
+        msg['date'] = get_now()
+        self.history_texts.append(msg)
+        for client in self.clients:
+            self.send_client_texts(client, [msg])
         
     def send_client_texts(self, client, texts):
         reply = {}
@@ -38,7 +87,11 @@ class Instance(object):
         reply['messages'] = texts
         client.send(json.dumps(reply))
         
-
+        
+def get_now():
+    now = datetime.datetime.utcnow()
+    return now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        
 def get_instance(instanceId):
     if INSTANCES.get(instanceId) is None:
         print "Creating instance for", instanceId
--- app/websocket.py
+++ app/websocket.py
@@ -6,7 +6,6 @@ def handle_websocket(ws):
     instance = None
     while True:
         message = ws.receive()
-        print "Handling message", message
         if message is None:
             if instance is not None:
                 instance.remove_client(ws)
@@ -15,7 +14,6 @@ def handle_websocket(ws):
             message = json.loads(message)
             if instance is None:
                 instance = get_instance(message['instanceId'])
-                print "Assigning", ws, "to", message['instanceId'], instance
-                instance.add_client(ws)
+                instance.add_client(ws, message)
             instance.on_client_message(ws, message)
             
